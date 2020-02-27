8e7379fa4d2cc227d2a0ba0f3d2c5b45d45edef9
GitHub <809002214@qq.com>
2020-02-27 10:26:32 +0800

    提交缺少虚拟机API配置类

---
+++ Common/VSphereConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moetech.Zhuangzhou.Common
+{
+    /// <summary>
+    /// vSphere配置
+    /// </summary>
+    public class VSphereConfig
+    {
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// URL
+        /// </summary>
+        public string SessionURl { get; set; }
+    }
+}
---
+++ Interface/IVsphere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vSphereRestWrapper.CIS;
+using vSphereRestWrapper.VCenter;
+
+namespace Moetech.Zhuangzhou.Interface
+{
+    /// <summary>
+    /// vSphere接口
+    /// </summary>
+    public interface IVsphere
+    {
+        /// <summary>
+        /// 连接
+        /// </summary>
+        Task<bool> Connect(string sessionURl, string userName, string password);
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="sessionId"></param>
+        Task<List<VMItem>> ListAsync();
+    }
+}
