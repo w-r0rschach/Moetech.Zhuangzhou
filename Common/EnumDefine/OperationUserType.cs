513c160a3cdf2fd62d9d8554519dc8a23a880c25
rendelang <rendelang@scgathersoft.com>
2020-02-28 16:54:43 +0800

    添加用户操作类型

---
+++ Common/EnumDefine/OperationUserType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moetech.Zhuangzhou.Common.EnumDefine
+{
+    /// <summary>
+    /// 用户操作类型
+    /// </summary>
+    public enum OperationUserType
+    {
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        USERNAME=0,
+        /// <summary>
+        /// 工号
+        /// </summary>
+        WORKNUMBER=1
+    }
+}
