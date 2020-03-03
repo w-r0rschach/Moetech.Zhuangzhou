f0f21998e9af8224f45a79f3c1ccf4e9fb310a07
zhangwenyiname <1335370124@qq.com>
2020-03-03 14:55:57 +0800

    添加消息提醒类

---
+++ Models/MessageWarn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moetech.Zhuangzhou.Models
+{
+    public class MessageWarn
+    {
+        /// <summary>
+        /// 消息主键
+        /// </summary>
+        [Key]
+        
+        public int MessageId { set; get; }
+
+        /// <summary>
+        /// 消息主题
+        /// </summary>
+        [StringLength(100)]
+        [Display(Name = "消息主题")]
+        public string MessageTitle { set; get; }
+
+        /// <summary>
+        /// 消息内容
+        /// </summary>
+        [StringLength(255)]
+        [Display(Name = "消息内容")]
+        public string MessageContent { set; get; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        [Display(Name = "用户ID")]
+        public int PonsonalId { set; get; }
+
+        /// <summary>
+        /// 提醒时间
+        /// </summary>
+        [Display(Name = "提醒时间")]
+        public DateTime MessageWarnDate { set; get; }
+
+        /// <summary>
+        /// (消息类型)是否已读 0：未读  1：已读
+        /// </summary>
+        [Display(Name = "消息类型")]
+        public int MessageType { set; get; }
+
+        /// <summary>
+        /// 已读时间
+        /// </summary>
+        [Display(Name = "已读时间")]
+        public DateTime MessageReadDate { set; get; }
+    }
+}
